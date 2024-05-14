9c3d9a34bf5417f8cd42e068ee797343b813e609
Daniil Zahoruiko <daniel.zagoruyko@gmail.com>
2024-05-14 00:20:47 -0600

    Panorama conversion changes

--- utils.py
+++ utils.py
@@ -3,8 +3,10 @@ from PIL import Image
 from io import BytesIO
 import requests
 import threading
-from Equirectangular import Equirectangular
 import matplotlib.image
+import py360convert
+import imageio
+from scipy.ndimage import map_coordinates
 
 URL = "https://streetviewpixels-pa.googleapis.com/v1/tile?cb_client=apiv3&panoid={}&output=tile&x={}&y={}&zoom={}&nbt=1&fover=2"
 
@@ -33,8 +35,62 @@ def fetch_image(topleft, bottomright, panoId, zoom, heading, pitch, cache):
         for t in threads:
             t.join()
         if width / height != 2:
-            img = img[:width / 2 - height, :, :] # trim the black rectangle at the bottom
+            img = img[:int(width * 256), :, :] # trim the black rectangle at the bottom
         cache.set(panoId, img)
-    equ = Equirectangular(img)
-    matplotlib.image.imsave('img.jpg', equ.GetPerspective(120, heading, pitch, 1080, 1920))
-    return img
+    # imageio.imsave('img.jpg', panorama_to_plane(img, 127, (2560, 1271), heading - 90, 90 - pitch))
+    perspective = panorama_to_plane(img, 127, (2560, 1271), heading - 90, 90 - pitch)
+    return perspective
+
+def map_to_sphere(x, y, z, yaw_radian, pitch_radian):
+
+
+    theta = np.arccos(z / np.sqrt(x ** 2 + y ** 2 + z ** 2))
+    phi = np.arctan2(y, x)
+
+    # Apply rotation transformations here
+    theta_prime = np.arccos(np.sin(theta) * np.sin(phi) * np.sin(pitch_radian) +
+                            np.cos(theta) * np.cos(pitch_radian))
+
+    phi_prime = np.arctan2(np.sin(theta) * np.sin(phi) * np.cos(pitch_radian) -
+                           np.cos(theta) * np.sin(pitch_radian),
+                           np.sin(theta) * np.cos(phi))
+    phi_prime += yaw_radian
+    phi_prime = phi_prime % (2 * np.pi)
+
+    return theta_prime.flatten(), phi_prime.flatten()
+
+
+def interpolate_color(coords, img, method='bilinear'):
+    order = {'nearest': 0, 'bilinear': 1, 'bicubic': 3}.get(method, 1)
+    red = map_coordinates(img[:, :, 0], coords, order=order, mode='reflect')
+    green = map_coordinates(img[:, :, 1], coords, order=order, mode='reflect')
+    blue = map_coordinates(img[:, :, 2], coords, order=order, mode='reflect')
+    return np.stack((red, green, blue), axis=-1)
+
+
+def panorama_to_plane(panorama, FOV, output_size, yaw, pitch):
+    pano_height, pano_width, _ = panorama.shape
+    yaw_radian = np.radians(yaw)
+    pitch_radian = np.radians(pitch)
+
+    W, H = output_size
+    f = (0.5 * W) / np.tan(np.radians(FOV) / 2)
+
+    u, v = np.meshgrid(np.arange(W), np.arange(H), indexing='xy')
+
+    x = u - W / 2
+    y = H / 2 - v
+    z = f
+
+    theta, phi = map_to_sphere(x, y, z, yaw_radian, pitch_radian)
+
+    U = phi * pano_width / (2 * np.pi)
+    V = theta * pano_height / np.pi
+
+    U, V = U.flatten(), V.flatten()
+    coords = np.vstack((V, U))
+
+    colors = interpolate_color(coords, panorama)
+    output_image = Image.fromarray(colors.reshape((H, W, 3)).astype('uint8'), 'RGB')
+
+    return output_image
