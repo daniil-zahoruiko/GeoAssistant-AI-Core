f0a0980ebb5cdce6f5d015794e56450b2ada2e37
Daniil Zahoruiko <daniel.zagoruyko@gmail.com>
2024-05-14 02:07:46 -0600

    Added a tool to save images

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask, request
 from flask_cors import CORS, cross_origin
 from flask_caching import Cache
-from utils import fetch_image
+from utils import fetch_image, save_image
 import math
 
 app = Flask(__name__)
@@ -28,6 +28,26 @@ def get_image_objects():
     fetch_image((0, 0), boundaries, panoid, zoom, currentHeading - originHeading, currentPitch + originPitch, cache)
     return [], 200
 
+@app.route("/imsave")
+@cross_origin()
+def imsave():
+    # TODO: Add screen width and screen height for panorama conversion (for now set by hand in utils)
+    tileWidth = (int(request.args.get("tileWidth")))
+    tileHeight = (int(request.args.get("tileHeight")))
+    worldWidth = (int(request.args.get("worldWidth")))
+    worldHeight = (int(request.args.get("worldHeight")))
+    panoid = request.args.get("panoId")
+    originHeading = (float(request.args.get("originHeading")))
+    originPitch = (float(request.args.get("originPitch")))
+    currentHeading = (float(request.args.get("currentHeading")))
+    currentPitch = (float(request.args.get("currentPitch")))
+    zoom = (int(request.args.get("zoom")))
+
+    boundaries = (math.ceil(worldWidth / tileWidth / 2) - 1, math.ceil(worldHeight / tileHeight / 2) - 1)
+    img = fetch_image((0, 0), boundaries, panoid, zoom, currentHeading - originHeading, currentPitch + originPitch, cache)
+    save_image(img)
+
+    return [], 200
 
 if __name__ == "__main__":
     app.run()
--- utils.py
+++ utils.py
@@ -3,6 +3,8 @@ from PIL import Image
 from io import BytesIO
 import requests
 import threading
+import os
+import imageio
 from scipy.ndimage import map_coordinates
 
 URL = "https://streetviewpixels-pa.googleapis.com/v1/tile?cb_client=apiv3&panoid={}&output=tile&x={}&y={}&zoom={}&nbt=1&fover=2"
@@ -91,3 +93,18 @@ def panorama_to_plane(panorama, FOV, output_size, yaw, pitch):
     output_image = Image.fromarray(colors.reshape((H, W, 3)).astype('uint8'), 'RGB')
 
     return output_image
+
+def save_image(img):
+    PATH_TO_DIRECTORY = "images"  # path starting from the root of repo
+    FILENAME = "img"    # base filename
+
+    path = os.path.join(os.getcwd(), PATH_TO_DIRECTORY)
+    if not os.path.exists(path):
+        os.mkdir(path)
+
+    filenames = [f for f in os.listdir(path) if os.path.isfile(os.path.join(path, f)) and f[-3:] == "jpg" and f.startswith(FILENAME + '-') and f[len(FILENAME) + 1:-4].isnumeric()]
+    numbers = [int(f[len(FILENAME) + 1:-4]) for f in filenames]
+    number = 1
+    if len(numbers) > 0:
+        number = max(numbers)
+    imageio.imsave(os.path.join(path, FILENAME + '-' + str(number) + '.jpg'), img)
