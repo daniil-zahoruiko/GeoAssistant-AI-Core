bb079bebe18ff0f6c7707751faedb3a388a72b21
daniil-zahoruiko <daniel.zagoruyko@gmail.com>
2024-08-19 03:24:47 -0600

    Update model

--- helpers/Detector.py
+++ helpers/Detector.py
@@ -1,4 +1,6 @@
 import torch
+import torch.nn.functional as F
+from torchvision import transforms
 from ultralytics import YOLO
 
 class SubModel:
@@ -7,12 +9,12 @@ class SubModel:
         self.model = model
         self.device = "cuda:0" if torch.cuda.is_available() else "cpu"
 
-        self.model.predict(None, device=self.device)
-
     def predict(self, img, coords):
-        cropped_img = img.crop(coords)
-        cropped_img.resize(self.imgsz)
-        return self.model.predict(cropped_img, device=self.device, imgsz=self.imgsz)[0].probs.top1
+        cropped_img = img.convert("RGB").crop(coords)
+        cropped_img = cropped_img.resize(self.imgsz)
+        tensor = transforms.ToTensor()(cropped_img)
+        prediction = F.softmax(self.model(tensor.to(self.device).unsqueeze(0)), dim=1)
+        return prediction.argmax(dim=1).cpu().tolist()[0]
     
 class Detector:
 
@@ -23,13 +25,13 @@ class Detector:
 
         self.classification_models = {}
         for model in model_config:
-            self.classification_models[model['key']] = SubModel(tuple(model['size']), YOLO(model['path']))
+            self.classification_models[model['key']] = SubModel(tuple(model['size']), torch.jit.load(model['path']))
 
         # Warm-up model
         self.model.predict(None, device=self.device)
 
     def bounding_boxes(self, img):
-        res = self.model.predict(img, device=self.device, conf=0.8, imgsz=(1088, 1920))
+        res = self.model.predict(img, device=self.device, conf=0.5, imgsz=(1088, 1920))
         boxes = []
         for box in res[0].boxes:
             cls = self.classification_models["bollard"].predict(img, box.xyxy.tolist()[0])
