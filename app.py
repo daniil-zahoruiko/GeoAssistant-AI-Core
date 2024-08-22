7e5646ea5599327397d0908c932a1e4cb14635a1
daniil-zahoruiko <daniel.zagoruyko@gmail.com>
2024-08-22 02:36:09 -0600

    Fix paths

--- app.py
+++ app.py
@@ -9,13 +9,15 @@ import os
 app = Flask(__name__)
 CORS(app)
 
-MAPPING_FILE_PATH = os.path.join(app.static_folder, 'mapping.json')
-MODEL_CONFIG_PATH = os.path.join(app.static_folder, 'model_cfg.json')
+CLASSIFICATION_MAPPING_FILE_PATH = os.path.join(app.static_folder, 'classification_mapping.json')
+DETECTION_MAPPING_FILE_PATH = os.path.join(app.static_folder, 'detection_mapping.json')
+SUBMODELS_CONFIG_PATH = os.path.join(app.static_folder, 'submodels_cfg.json')
 
-MAPPING_JSON = JSONLoader.load(MAPPING_FILE_PATH)
-MODEL_CONFIG = JSONLoader.load(MODEL_CONFIG_PATH)
+CLASSIFICATION_MAPPING = JSONLoader.load(CLASSIFICATION_MAPPING_FILE_PATH)
+DETECTION_MAPPING = JSONLoader.load(DETECTION_MAPPING_FILE_PATH)
+SUBMODELS_CONFIG = JSONLoader.load(SUBMODELS_CONFIG_PATH)
 
-detector = Detector(os.path.join(os.getcwd(), "model/best.pt"), MAPPING_JSON, MODEL_CONFIG)
+detector = Detector(os.path.join(os.getcwd(), "model/best.pt"), DETECTION_MAPPING, CLASSIFICATION_MAPPING, SUBMODELS_CONFIG)
 image_helper = ImageHelper()
 
 @app.route("/update", methods=["POST"])
