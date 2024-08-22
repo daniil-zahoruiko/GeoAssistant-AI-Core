c8151c89c6c6e97546be8bf9ecf69489dd71b69f
daniil-zahoruiko <daniel.zagoruyko@gmail.com>
2024-08-22 02:35:41 -0600

    Preparation to add more objects to detection, performance improvements

--- helpers/Detector.py
+++ helpers/Detector.py
@@ -4,10 +4,11 @@ from ultralytics import YOLO
 from model.SubModels import BollardNet
 
 class SubModel:
-    def __init__(self, model_class, imgsz, state_dict):
+    def __init__(self, model_class, imgsz, state_dict, metadata):
         self.imgsz = imgsz
+        self.metadata = metadata
         self.device = "cuda:0" if torch.cuda.is_available() else "cpu"
-        
+
         self.model = model_class()
         self.model.load_state_dict(state_dict)
         self.model.to(self.device)
@@ -16,21 +17,32 @@ class SubModel:
         cropped_img = img.convert("RGB").crop(coords)
         cropped_img = cropped_img.resize(self.imgsz)
         tensor = transforms.ToTensor()(cropped_img)
+
         prediction = self.model(tensor.unsqueeze(0).to(self.device))
-        return prediction.argmax(dim=1).cpu().tolist()[0]
+        res = prediction.argmax(dim=1).cpu().tolist()[0]
+        metadata = self.metadata[str(res)]
+        
+        return { 
+            'cls': res,
+            'name': metadata['name'],
+            'description': metadata['description']
+        }
     
 class Detector:
 
-    def __init__(self, file, mapping, model_config):
+    def __init__(self, file, detection_mapping, classification_mapping, submodels_config):
         CLASSIFICATION_MODEL_CLASSES = { "bollard": BollardNet }
 
         self.model = YOLO(file)
         self.device = "cuda:0" if torch.cuda.is_available() else "cpu"
-        self.mapping = mapping
+        self.mapping = detection_mapping
 
         self.classification_models = {}
-        for model in model_config:
-            self.classification_models[model['key']] = SubModel(CLASSIFICATION_MODEL_CLASSES[model['key']], tuple(model['size']), torch.load(model['path']))
+        for model in submodels_config:
+            self.classification_models[model['key']] = SubModel(CLASSIFICATION_MODEL_CLASSES[model['key']], 
+                                                                tuple(model['size']), 
+                                                                torch.load(model['path']), 
+                                                                classification_mapping[model['key']])
 
         # Warm-up model
         self.model.predict(None, device=self.device)
@@ -39,11 +51,10 @@ class Detector:
         res = self.model.predict(img, device=self.device, conf=0.7, imgsz=(1088, 1920))
         boxes = []
         for box in res[0].boxes:
-            cls = self.classification_models["bollard"].predict(img, box.xyxy.tolist()[0])
-            map = [item for item in self.mapping if item["cls"] == str(cls)]
-            boxes.append({  "cls": cls,
-                            "name": map[0]["name"],
-                            "description":map[0]["description"],
+            res = self.classification_models[self.mapping[str(int(box.cls.tolist()[0]))]].predict(img, box.xyxy.tolist()[0])
+            boxes.append({  "cls": res['cls'],
+                            "name": res['name'],
+                            "description":res['description'],
                             "coords": box.xyxy.tolist()[0]
                         })
         # print(boxes)
